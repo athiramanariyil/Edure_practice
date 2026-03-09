688d031a774b84f55b99af71443ae50cb4e77586
athiramanariyil <168619045+athiramanariyil@users.noreply.github.com>
2026-03-09 20:50:28 +0530

    text verification selenium

---
+++ text_verification.java
@@ -0,0 +1,22 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class text_verification {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		driver = new ChromeDriver();
+		String Exptext1="brainstorming";
+		driver.get("https://chatgpt.com/");
+		String ActText1=driver.getPageSource();
+		if(ActText1.contains(Exptext1)) {
+			System.out.println("Text available");
+		}else {
+			System.out.println("Text  not available");
+		}
+		driver.quit();
+	}
+
+}
